117c2cf05ac0e4f5d7ffecd44ea4080e4026d84d
Lawrence5296 <lawrence5296@gmail.com>
2017-02-05 01:24:41 +0900

    Delete table package and Create Router.

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component }          from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">トップ/出欠表</a>
+      <a routerLink="/heroes" routerLinkActive="active">幹事用ページ</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Alcoholic Manager';
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/in-memory-data.service.js
@@ -0,0 +1,28 @@
+"use strict";
+var InMemoryDataService = (function () {
+    function InMemoryDataService() {
+    }
+    InMemoryDataService.prototype.createDb = function () {
+        var heroes = [
+            { id: 1, name: 'Mr. Nice' },
+            { id: 2, name: 'Narco' },
+            { id: 3, name: 'Bombasto' },
+            { id: 4, name: 'Celeritas' },
+            { id: 5, name: 'Magneta' },
+            { id: 6, name: 'RubberMan' },
+            { id: 7, name: 'Dynama' },
+            { id: 8, name: 'Dr IQ' },
+            { id: 9, name: 'Magma' },
+            { id: 10, name: 'Tornado' }
+        ];
+        return { heroes: heroes };
+    };
+    return InMemoryDataService;
+}());
+exports.InMemoryDataService = InMemoryDataService;
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=in-memory-data.service.js.map
